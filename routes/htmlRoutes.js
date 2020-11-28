0dc1476c5a328be8599d4a92614fa08d5f5cb30e
Ben Galloway <bdgalloway85@gmail.com>
2020-11-28 17:39:16 -0600

    htmlRoutes setup

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,14 @@
 const path = require('path');
 const router = require('express').Router();
 
+router.get('/notes', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+    console.log(__dirname);
+});
 
 router.get('*', (_req, res) => {
     //res.send(__dirname);
-    res.sendFile(__dirname, '../public/index.html');
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
